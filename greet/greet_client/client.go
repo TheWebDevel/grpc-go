0b49304ff6f1756f6b5f80f569e616f19b2d1f70
TheWebDevel <sathish@skcript.com>
2020-06-03 21:07:14 +0530

    unary API client implementation

--- greet/greet_client/client.go
+++ greet/greet_client/client.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"groc-go/greet/greetpb"
 	"log"
@@ -21,5 +22,24 @@ func main() {
 	defer cc.Close()
 
 	c := greetpb.NewGreetServiceClient(cc)
-	fmt.Printf("Created the client: %f", c)
+
+	doUnary(c)
+
+}
+
+func doUnary(c greetpb.GreetServiceClient) {
+	fmt.Println("Starting to do Unary RPC...")
+	req := &greetpb.GreetRequest{
+		Greeting: &greetpb.Greeting{
+			FirstName: "Sathish",
+			LastName:  "S",
+		},
+	}
+	res, err := c.Greet(context.Background(), req)
+
+	if err != nil {
+		log.Fatalf("Error from server: %v", err)
+	}
+
+	log.Printf("Response from greet: %v", res)
 }
